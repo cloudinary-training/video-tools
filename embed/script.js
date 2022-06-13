51c632b4348da023a97727dd7b64202455aba3b1
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-06-13 09:39:53 -0700

    add playback rates and test page

--- embed/script.js
+++ embed/script.js
@@ -5,7 +5,7 @@ document.addEventListener('submit', (e) => {
   const cloudname = document.getElementById('cloudname').value;
   const captionfile = document.getElementById('captionurl').value;
 
-  const iframe = `<iframe src="https://player.cloudinary.com/embed/?public_id=${vidPublicId}&cloud_name=${cloudname}&player[fluid]=true&player[controls]=true&source[text_tracks][captions][label]=English&source[text_tracks][captions][language]=en&source[text_tracks][captions][url]=${captionfile}&source[text_tracks][captions][default]=false&source[text_tracks][subtitles][label]=English&source[subtitles][language]=en&source[subtitles][url]=${captionfile}&source[subtitles][default]=false&source[source_types][0]=dash&source[source_types][1]=hls&source[source_types][2]=mp4" width="1024" height="576" allow="autoplay;fullscreen;encrypted-media;picture-in-picture" allowfullscreen frameborder="0"></iframe>`;
+  const iframe = `<iframe src="https://player.cloudinary.com/embed/?public_id=${vidPublicId}&cloud_name=${cloudname}&player[fluid]=true&player[controls]=true&player[playbackRates][0]=0.5&player[playbackRates][1]=1&player[playbackRates][2]=2&source[text_tracks][captions][label]=English&source[text_tracks][captions][language]=en&source[text_tracks][captions][url]=${captionfile}&source[text_tracks][captions][default]=false&source[text_tracks][subtitles][label]=English&source[subtitles][language]=en&source[subtitles][url]=${captionfile}&source[subtitles][default]=false&source[source_types][0]=dash&source[source_types][1]=hls&source[source_types][2]=mp4" width="1024" height="576" allow="autoplay;fullscreen;encrypted-media;picture-in-picture" allowfullscreen frameborder="0"></iframe>`;
 
   document.getElementById('iframe').value = iframe;
 });
